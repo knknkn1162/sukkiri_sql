af51fc08f07d8ce4ab296c9adfd47ce1ffe3d140
Kenta_Nakajima <knknkn1162@gmail.com>
2023-10-08 14:23:02 +0900

    1.2/ch8

--- C1.2/ch8.sql
+++ C1.2/ch8.sql
@@ -8,6 +8,7 @@ select
 from 注文
 join 商品
 on 商品.商品コード = 注文.商品コード
+where 注文番号 = '202201130115'
 
 -- 62
 select 注文日, 注文番号, 注文枝番, 数量,
@@ -15,7 +16,8 @@ select 注文日, 注文番号, 注文枝番, 数量,
 from 注文 as a
 join 廃番商品 as b
 on a.商品コード = b.商品コード
-where a.商品コード = 'A0009'
+where a.商品コード = 'A0009' and
+    注文日 > 廃番日
 
 -- 63
 select
@@ -70,10 +72,22 @@ from (
     union all
     select 商品コード, 商品名, 単価,商品区分,廃番日
     from 廃番商品
-    
 ) as a
 left join 注文 as b
 on a.商品コード = b.商品コード
+
+-- 67.2
+SELECT
+    T.注文日,
+    COALESCE(
+        S.商品コード ||':'||S.商品名,
+        T.商品コード ||'(:廃番済み)'
+    ) AS 商品,
+    COALESCE(T. 数量 , 0) AS 数量
+FROM 注文 AS T
+FULL JOIN 商品 AS S
+ON T.商品コード = S. 商品コード 
+WHERE S. 商品区分 = '3'
 
 -- 68
 select 注文日, 注文番号, 注文枝番,
@@ -97,11 +111,12 @@ where 注文番号 = '202104030010'
 select 
     a.商品コード,
     商品名,
-    単価,
+    coalesce(単価,0) as 単価,
     b.数量,
     単価 * b.数量 as 総売上
 from 商品 as a
-join (
+-- a.商品コード like 'B%'に該当するものすべて関連づける
+left join (
     select 商品コード,
         sum(数量) as 数量
     from 注文
@@ -110,3 +125,10 @@ join (
 on a.商品コード = b.商品コード
 where a.商品コード like 'B%'
 order by a.商品コード
+
+-- 70
+select a.商品名, a.商品コード,a.関連商品コード,
+    b.商品名 as 関連商品名
+from 商品 as a
+join 商品 as b
+on b.商品コード = a.関連商品コード
